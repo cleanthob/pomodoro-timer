b4234ba42c0ff8df4dc3c06366da970c51cf3286
Cleantho <cleanthobeltrao@gmail.com>
2025-11-11 10:10:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { JSX } from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
+
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <PomodoroTimer
+        pomodoroTime={10}
+        shortRestTime={2}
+        longRestTime={5}
+        cycles={4}
+      />
+    </div>
+  );
+}
+
+export default App;
